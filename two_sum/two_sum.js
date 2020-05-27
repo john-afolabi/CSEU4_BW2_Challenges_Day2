af76c4070954775b60494eb89b934ad3ab0778c7
John Afolabi <johnafolabi.da@gmail.com>
2020-05-27 13:25:53 +0100

    Complete two sum js

--- two_sum/two_sum.js
+++ two_sum/two_sum.js
@@ -3,6 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+	const index = {};
+	var result = [];
+	nums.forEach((num, i) => {
+		if (target - num in index) {
+			result = [i, index[target - num]];
+		} else {
+			index[num] = i;
+		}
+	});
+	return result;
 };
